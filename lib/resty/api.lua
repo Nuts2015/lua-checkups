dfa2a70ba21275232c632aaeaa9640b8ffccaa46
vickim <liuwg2015@163.com>
2021-12-30 15:17:57 +0800

    add code first

---
+++ lib/resty/api.lua
@@ -0,0 +1,14 @@
+-- local _M = {}
+-- function _M.greet(name)
+--     ngx.say("Greetings from ", name)
+-- end
+
+-- function _M.get_proxy()
+--     ngx.say("url:","api.eosargentina.io")
+-- end
+-- return _M
+
+
+local uri = "http://api.eosargentina.io";
+
+return uri;
