3dbe7e9f37de59e216cfc4c9e2f0e01fbd0475fb
vickim <liuwg2015@163.com>
2021-12-30 17:03:18 +0800

    add rand rate

--- lib/resty/api.lua
+++ lib/resty/api.lua
@@ -3,17 +3,28 @@ local config = require "config.config"
 local _M = {}
 function _M.get_proxy()
     local server = _M.get_rand_server()
-    local url = server["host"]
+    local url = server.host
 
-    return "http://api.eosargentina.io"
+    return url
 end
 
 function _M.get_rand_server()
-    local count = 0
     local server_list = config.servers
-    for k, v in pairs(server_list) do count = count + 1 end
-    local num = math.random(0, (count - 1))
-    return server_list[num]
+    local ran = math.random(1, 100)
+    local rate = 0
+    local index = 1
+    for i, value in pairs(server_list) do
+        rate = rate + value.weight
+        if ran < rate then
+            index = i
+            break
+        end
+    end
+    return server_list[index]
+
+    -- local length = #server_list
+    -- local ri = math.random(1, length)
+    -- return server_list[ri]
 end
 
 return _M
